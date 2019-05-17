9218eb8cb34e330b3d647709a770f9e0912335cd
john.tan <shrimpcolo@gmail.com>
2019-05-17 16:26:02 +0800

    Make the RecyclerView Adapter work with a PagedList

--- android-paging-master/app/src/main/java/com/example/android/codelabs/paging/ui/ReposAdapter.kt
+++ android-paging-master/app/src/main/java/com/example/android/codelabs/paging/ui/ReposAdapter.kt
@@ -17,14 +17,15 @@
 package com.example.android.codelabs.paging.ui
 
 import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.android.codelabs.paging.model.Repo
 
 /**
  * Adapter for the list of repositories.
  */
-class ReposAdapter : ListAdapter<Repo, androidx.recyclerview.widget.RecyclerView.ViewHolder>(REPO_COMPARATOR) {
+class ReposAdapter : PagedListAdapter<Repo, RecyclerView.ViewHolder>(REPO_COMPARATOR) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): androidx.recyclerview.widget.RecyclerView.ViewHolder {
         return RepoViewHolder.create(parent)
